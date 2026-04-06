3ae35d9d35589930fe1033746b314cd307be13c7
luseeee-star <13726466057@163.com>
2026-04-06 23:35:28 +0800

    Initial commit

---
+++ src/main/java/lsj/qg/finaltrain/FinalTrainApplication.java
@@ -0,0 +1,13 @@
+package lsj.qg.finaltrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lsj/qg/finaltrain/FinalTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package lsj.qg.finaltrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
